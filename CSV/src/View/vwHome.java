5d59bea15ca8c163a8fd132068cb7bce8dbde2ee
Marcelo Vieira <dj.marcelo.2009@gmail.com>
2019-01-17 13:31:11 -0200

    Export OK

--- CSV/src/View/vwHome.java
+++ CSV/src/View/vwHome.java
@@ -33,8 +33,8 @@ public class vwHome extends JFrame {
 	private JButton bntMontar, btnLimpar, btnSalvar;
 	private JTextArea txtArea, txtQ, txtA;
 	private JScrollPane scrollQ, scrollA, scrollArea;
-	private String [] ae = {"A) ","B) ","C) ","D) ","E) "};
-	private String [] eliminar = {"A ","B ","C ","D ","E "};
+	private String[] ae = { "A) ", "B) ", "C) ", "D) ", "E) " };
+	private String[] eliminar = { "A ", "B ", "C ", "D ", "E " };
 	private ArrayList<String> listaAlternativas = new ArrayList<String>();
 	private JButton btnSair;
 
@@ -64,32 +64,32 @@ public class vwHome extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		lblQ = new JLabel("Q:");
 		lblQ.setHorizontalAlignment(SwingConstants.RIGHT);
 		lblQ.setBounds(851, 19, 63, 14);
 		contentPane.add(lblQ);
-		
+
 		lblA = new JLabel("A:");
 		lblA.setHorizontalAlignment(SwingConstants.RIGHT);
 		lblA.setBounds(851, 43, 63, 14);
 		contentPane.add(lblA);
-		
+
 		JLabel lblInsertQuestesSistemasvox = new JLabel("INSERT Quest\u00F5es, SistemasVOX.");
 		lblInsertQuestesSistemasvox.setBounds(76, -5, 704, 57);
 		lblInsertQuestesSistemasvox.setHorizontalAlignment(SwingConstants.CENTER);
 		lblInsertQuestesSistemasvox.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 18));
 		contentPane.add(lblInsertQuestesSistemasvox);
-		
+
 		txtArea = new JTextArea();
 		txtArea.setWrapStyleWord(true);
 		txtArea.setToolTipText("Quest\u00E3o Montada.");
 		txtArea.setFont(new Font("Monospaced", Font.BOLD, 14));
 		txtArea.setLineWrap(true);
-		
+
 		contentPane.add(this.scrollArea = new JScrollPane(txtArea));
 		scrollArea.setBounds(10, 319, 713, 354);
-		
+
 		btnSalvar = new JButton("Salvar Quest\u00E3o");
 		btnSalvar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -98,7 +98,7 @@ public class vwHome extends JFrame {
 		});
 		btnSalvar.setBounds(745, 423, 159, 46);
 		contentPane.add(btnSalvar);
-		
+
 		btnLimpar = new JButton("Limpar Campos");
 		btnLimpar.setBounds(745, 319, 159, 46);
 		btnLimpar.addActionListener(new ActionListener() {
@@ -107,25 +107,25 @@ public class vwHome extends JFrame {
 			}
 		});
 		contentPane.add(btnLimpar);
-		
+
 		txtQ = new JTextArea();
 		txtQ.setFont(new Font("Monospaced", Font.ITALIC, 14));
 		txtQ.setLineWrap(true);
 		txtQ.setWrapStyleWord(true);
 		txtQ.setToolTipText("Informe o Enunciado");
-		
+
 		contentPane.add(this.scrollQ = new JScrollPane(txtQ));
 		scrollQ.setBounds(10, 60, 907, 65);
-		
+
 		txtA = new JTextArea();
 		txtA.setFont(new Font("Monospaced", Font.BOLD, 14));
 		txtA.setLineWrap(true);
 		txtA.setWrapStyleWord(true);
 		txtA.setToolTipText("Informe as Alternativas?");
-		
+
 		contentPane.add(this.scrollA = new JScrollPane(txtA));
 		scrollA.setBounds(10, 136, 907, 165);
-		
+
 		bntMontar = new JButton("Montar Quest\u00E3o");
 		bntMontar.setBounds(745, 371, 159, 46);
 		bntMontar.addActionListener(new ActionListener() {
@@ -134,7 +134,7 @@ public class vwHome extends JFrame {
 			}
 		});
 		contentPane.add(bntMontar);
-		
+
 		JButton btnListar = new JButton("Listar Tudo.");
 		btnListar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -143,7 +143,7 @@ public class vwHome extends JFrame {
 		});
 		btnListar.setBounds(745, 476, 159, 46);
 		contentPane.add(btnListar);
-		
+
 		JButton btnExpor = new JButton("Exportar CSV");
 		btnExpor.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -152,7 +152,7 @@ public class vwHome extends JFrame {
 		});
 		btnExpor.setBounds(745, 533, 159, 46);
 		contentPane.add(btnExpor);
-		
+
 		btnSair = new JButton("Sair");
 		btnSair.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -168,15 +168,16 @@ public class vwHome extends JFrame {
 	protected void exportarCSV() {
 		ArrayList<Questoes> questoes = new ArrayList<Questoes>();
 		ArrayList<Alternativa> alternativas = new ArrayList<Alternativa>();
-		
+
 		for (int i = 1; i <= Integer.parseInt(Controladora.consultarTotalQ()); i++) {
 			questoes.add(Controladora.consultarQuestao(String.valueOf(i)));
 		}
-		
+
 		for (int i = 1; i <= Integer.parseInt(Controladora.consultarTotalA()); i++) {
 			alternativas.add(Controladora.consultarAlternativa(String.valueOf(i)));
 		}
-		 exportarQ(questoes);
+		exportarQ(questoes);
+		exportarA(alternativas);
 	}
 
 	private void exportarQ(ArrayList<Questoes> questoes) {
@@ -185,58 +186,91 @@ public class vwHome extends JFrame {
 			StringBuilder sb = new StringBuilder();
 			String separador = ",";
 			String finalizador = "\r\n";
-			
-	        sb.append("cod");
-	        sb.append(separador);
-	        sb.append("enunciado");
-	        sb.append(separador);
-	        sb.append("referencia");
-	        //sb.append(separador);
-	        sb.append(finalizador);
+
+			sb.append("cod");
+			sb.append(separador);
+			sb.append("enunciado");
+			sb.append(separador);
+			sb.append("referencia");
+			sb.append(finalizador);
 
 			for (int i = 0; i < questoes.size(); i++) {
-		        sb.append( questoes.get(i).getCod());
-		        sb.append(separador);
-		        sb.append("'" + questoes.get(i).getEnunciado().replace("\n", "") + "'");
-		        sb.append(separador);
-		        sb.append("'" +  "vazio"   + "'" );
-		        //sb.append(separador);
-		        sb.append(finalizador);
+				sb.append(questoes.get(i).getCod());
+				sb.append(separador);
+				sb.append("'" + questoes.get(i).getEnunciado() + "'");
+				sb.append(separador);
+				sb.append("'" + questoes.get(i).getReferencia() + "'");
+				sb.append(finalizador);
 			}
-	        pw.write(sb.toString());
-	        pw.close();
-	        txtArea.setText("Exportado com Sucesso.");
+			pw.write(sb.toString());
+			pw.close();
+			txtArea.setText("Exportado com Sucesso.");
 
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
-		
+
+	}
+	
+	private void exportarA(ArrayList<Alternativa> alternativas) {
+		try {
+			PrintWriter pw = new PrintWriter(new File("Alternativas.csv"));
+			StringBuilder sb = new StringBuilder();
+			String separador = ",";
+			String finalizador = "\r\n";
+
+			sb.append("cod");
+			sb.append(separador);
+			sb.append("cod_q");
+			sb.append(separador);
+			sb.append("classificacao");
+			sb.append(separador);
+			sb.append("resposta");
+			sb.append(finalizador);
+
+			for (int i = 0; i < alternativas.size(); i++) {
+				sb.append(alternativas.get(i).getCod());
+				sb.append(separador);
+				sb.append(alternativas.get(i).getCod_q());
+				sb.append(separador);
+				sb.append(alternativas.get(i).getClassificacao());
+				sb.append(separador);
+				sb.append("'" + alternativas.get(i).getResposta()+ "'");
+				sb.append(finalizador);
+			}
+			pw.write(sb.toString());
+			pw.close();
+			txtArea.setText("Exportado com Sucesso.");
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+
 	}
 
 	protected void listarQuestoes() {
 		listartudo();
-		
 	}
 
 	private void listartudo() {
 		ArrayList<Questoes> questoes = new ArrayList<Questoes>();
 		String s = "";
-		
+
 		for (int i = 1; i <= Integer.parseInt(Controladora.consultarTotalQ()); i++) {
 			questoes.add(Controladora.consultarQuestao(String.valueOf(i)));
 		}
-		
+
 		for (int i = 0; i < questoes.size(); i++) {
-			s += questoes.get(i).getCod() +  ") " + questoes.get(i).getEnunciado() + "\n";
-			
-			ArrayList<Alternativa> alternativas = Controladora.getAlternativas(questoes.get(i).getCod());			
+			s += questoes.get(i).getCod() + ") " + questoes.get(i).getEnunciado() + "\n";
+
+			ArrayList<Alternativa> alternativas = Controladora.getAlternativas(questoes.get(i).getCod());
 			for (int j = 0; j < alternativas.size(); j++) {
 				s += ae[j] + alternativas.get(j).getResposta() + "\n";
 			}
 			s += "--------------------------------------------------------------------------------------\n";
 		}
-		
-		txtArea.setText(s.replace(": ", ":\n\n") + "FIM.\n");		
+
+		txtArea.setText(s.replace(": ", ":\n\n") + "FIM.\n");
 	}
 
 	private void bloquearBotao() {
@@ -245,15 +279,18 @@ public class vwHome extends JFrame {
 	}
 
 	protected void salvar() {
-		Controladora.savarQ(new Questoes((String.valueOf(Integer.parseInt(Controladora.consultarTotalQ()) + 1)), (txtQ.getText().replace("\n", " ") +":\n"), ""));
+		Controladora.savarQ(new Questoes((String.valueOf(Integer.parseInt(Controladora.consultarTotalQ()) + 1)),
+				(txtQ.getText().replace("\n", " ")), ""));
+		
 		for (int i = 0; i < listaAlternativas.size(); i++) {
-			Controladora.savarA(new Alternativa(String.valueOf(Integer.parseInt(Controladora.consultarTotalA()) + 1), String.valueOf(Integer.parseInt(Controladora.consultarTotalQ())), "0", listaAlternativas.get(i)));
+			Controladora.savarA(new Alternativa(String.valueOf(Integer.parseInt(Controladora.consultarTotalA()) + 1),
+					String.valueOf(Integer.parseInt(Controladora.consultarTotalQ())), "0", listaAlternativas.get(i)));
 		}
 		atualizarLBL();
 		limpar();
 		txtArea.setText("Salvo com Sucesso.");
 		bloquearBotao();
-		
+
 	}
 
 	protected void limpar() {
@@ -269,17 +306,18 @@ public class vwHome extends JFrame {
 			txtArea.setText("Poxa Vida, os Campos de Enunciado e Alternativas estão vazios.!!!!");
 		} else {
 			String s = "";
-			s += (String.valueOf(Integer.parseInt(Controladora.consultarTotalQ()) + 1)) + ") " + txtQ.getText().replace("\n", " ") +":\n";
+			s += (String.valueOf(Integer.parseInt(Controladora.consultarTotalQ()) + 1)) + ") "
+					+ txtQ.getText().replace("\n", " ") + "\n";
 			s += alternativasLimpa(txtA.getText());
 			txtArea.setText(s);
-			liberarbotao();		
+			liberarbotao();
 		}
 	}
 
 	private void liberarbotao() {
 		btnLimpar.setEnabled(true);
 		btnSalvar.setEnabled(true);
-		
+
 	}
 
 	private String alternativasLimpa(String texto) {
@@ -289,16 +327,17 @@ public class vwHome extends JFrame {
 		texto = texto.replace("\n", "");
 		alternativas = texto.split(Pattern.quote("."));
 		for (int i = 0; i < alternativas.length; i++) {
-			s += ("\n" + ae[i]+ alternativas[i].substring(2, alternativas[i].length()) + ".");
+			s += ("\n" + ae[i] + alternativas[i].substring(2, alternativas[i].length()) + ".");
 			listaAlternativas.add(alternativas[i].substring(2, alternativas[i].length()) + ".");
-			//System.out.println(alternativas[i].substring(2, alternativas[i].length()) + ".");
-			//System.out.println(alternativas[i]);
+			// System.out.println(alternativas[i].substring(2, alternativas[i].length()) +
+			// ".");
+			// System.out.println(alternativas[i]);
 		}
 		return s;
 	}
 
 	private void atualizarLBL() {
 		lblA.setText("A: " + Controladora.consultarTotalA() + ".");
-		lblQ.setText("Q: " + Controladora.consultarTotalQ() + ".");	
+		lblQ.setText("Q: " + Controladora.consultarTotalQ() + ".");
 	}
 }
