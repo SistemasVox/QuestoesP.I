28a5d14b5619e6cdba1febd84ab60eb8d2ddd6ad
Marcelo Vieira <dj.marcelo.2009@gmail.com>
2019-03-29 19:02:14 -0300

    Adcionado, justiticativa de cada questão. FIX Exit On Close

--- CSV/src/View/vwSalvarAlter.java
+++ CSV/src/View/vwSalvarAlter.java
@@ -56,7 +56,7 @@ public class vwSalvarAlter extends JFrame {
 		this.listaAlternativas = listaAlternativas;
 		this.vwHome = vwHome;
 		
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 758, 479);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
