9f268f2ea5b2e995ee5efa6d7e34ea978b63d0e3
Marcelo Vieira <dj.marcelo.2009@gmail.com>
2019-05-10 15:32:59 -0300

    ...

--- CSV/src/View/vwConteudo.java
+++ CSV/src/View/vwConteudo.java
@@ -61,7 +61,7 @@ public class vwConteudo extends JFrame {
 	 * Create the frame.
 	 */
 	public vwConteudo() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 717, 426);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
